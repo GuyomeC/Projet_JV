e950086680db921d305d4bb2adbca750bd992ec9
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2024-05-03 11:17:22 +0100

    Add follow object

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -2,9 +2,21 @@
 
 public class CameraProfile : MonoBehaviour
 {
+    [Header("Type")]
+    [SerializeField] private CameraProfileType _profileType = CameraProfileType.Static;
+
+    [Header("Follow")]
+    [SerializeField] private Transform _targetToFollow = null;
+
+    public CameraProfileType ProfileType => _profileType;
+
+    public Transform TargetToFollow => _targetToFollow;
+
     private Camera _camera;
     public float CameraSize => _camera.orthographicSize;
     public Vector3 Position => _camera.transform.position;
+
+    
 
     private void Awake()
     {
@@ -14,5 +26,13 @@ public class CameraProfile : MonoBehaviour
             _camera.enabled = false;
         }
     }
+
+
+}
+
+public enum CameraProfileType
+{
+    Static = 0,
+    FollowTarget
 }
 
